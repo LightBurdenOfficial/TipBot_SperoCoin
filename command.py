e846ea05eac751bcf7f7dce64b09310df7982243
DigitalCoinBRL <32315718+DigitalCoin1@users.noreply.github.com>
2021-02-02 15:19:20 -0300

    News and fixs
    [FIX] Preços de mercados
    [NEW] comando /getblocks
    [FIX] Tabs das linhas
    [FIX] Pequenas correções

--- command.py
+++ command.py
@@ -14,113 +14,140 @@ updater = Updater(token='BOT_TOKEN_HERE')
 dispatcher = updater.dispatcher
 
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-					level=logging.INFO)
+logging.basicConfig(filename='log.log',
+                                        filemode='w',
+                                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                        level=logging.DEBUG)
 
 def commands(bot, update):
-	user = update.message.from_user.username
-	bot.send_message(chat_id=update.message.chat_id, text="📖 List of commands: \n "+
-		" /hello \n /commands \n /recharge \n /pay \n /drawout \n /funds \n /price \n /help")
+        user = update.message.from_user.username
+        bot.send_message(chat_id=update.message.chat_id, text="📖 List of commands: \n "+
+                " /hello \n /commands \n /recharge \n /pay \n /drawout \n /funds \n /price \n /help")
 
 def help(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text=" /pay & /withdraw have a specfic format use them like so:"+
-		"\n Parameters: \n <user> = target user to pay \n <amount> = amount of SperoCoin to utilise \n <address> = SperoCoin address to withdraw to \n \n Tipping format: \n /pay <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
+        bot.send_message(chat_id=update.message.chat_id, text=" /pay & /drawout have a specfic format use them like so:"+
+                "\n Parameters: \n <user> = target user to pay \n <amount> = amount of SperoCoin to utilise \n <address> = SperoCoin address to withdraw to \n \n Tipping format: \n /pay <user> <amount> \n \n Withdrawing format: \n /drawout <address> <amount>")
 
 def recharge(bot, update):
-	user = update.message.from_user.username
-	if user is None:
-		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
-	else:
-		address = "/home/sperocoin/DigitalCoinBRL/src/SperoCoind"
-		result = subprocess.run([address,"getaccountaddress",user],stdout=subprocess.PIPE)
-		clean = (result.stdout.strip()).decode("utf-8")
-		bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
+        user = update.message.from_user.username
+        if user is None:
+                bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+        else:
+                address = "/usr/local/bin/SperoCoind"
+                extra1 = "-datadir=/coin/data"
+                extra2 = "-conf=/coin/sperocoin.conf"
+                result = subprocess.run([address,extra1,extra2,"getaccountaddress",user],stdout=subprocess.PIPE)
+                clean = (result.stdout.strip()).decode("utf-8")
+                bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
 
 def pay(bot,update):
-	user = update.message.from_user.username
-	target = update.message.text[5:]
-	amount =  target.split(" ")[1]
-	target =  target.split(" ")[0]
-	if user is None:
-		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
-	else:
-		machine = "@SperoCoinWalletBot"
-		if target == machine:
-			bot.send_message(chat_id=update.message.chat_id, text="HODL.")
-		elif "@" in target:
-			target = target[1:]
-			user = update.message.from_user.username
-			core = "/home/sperocoin/DigitalCoinBRL/src/SperoCoind"
-			result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
-			balance = float((result.stdout.strip()).decode("utf-8"))
-			amount = float(amount)
-			if balance < amount:
-				bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
-			elif target == user:
-				bot.send_message(chat_id=update.message.chat_id, text="You can't pay yourself!")
-			else:
-				balance = str(balance)
-				amount = str(amount)
-				tx = subprocess.run([core,"move",user,target,amount],stdout=subprocess.PIPE)
-				bot.send_message(chat_id=update.message.chat_id, text="@{0} paid @{1} of {2} SPERO".format(user, target, amount))
-		else:
-			bot.send_message(chat_id=update.message.chat_id, text="Error that user is not applicable.")
+        user = update.message.from_user.username
+        target = update.message.text[5:]
+        amount =  target.split(" ")[1]
+        target =  target.split(" ")[0]
+        if user is None:
+                bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+        else:
+                machine = "@SperoCoinWalletBot"
+                if target == machine:
+                        bot.send_message(chat_id=update.message.chat_id, text="HODL.")
+                elif "@" in target:
+                        target = target[1:]
+                        user = update.message.from_user.username
+                        core = "/usr/local/bin/SperoCoind"
+                        extra1 = "-datadir=/coin/data"
+                        extra2 = "-conf=/coin/sperocoin.conf"
+                        result = subprocess.run([core,extra1,extra2,"getbalance",user],stdout=subprocess.PIPE)
+                        balance = float((result.stdout.strip()).decode("utf-8"))
+                        amount = float(amount)
+                        if balance < amount:
+                                bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
+                        elif target == user:
+                                bot.send_message(chat_id=update.message.chat_id, text="You can't pay yourself!")
+                        else:
+                                balance = str(balance)
+                                amount = str(amount)
+                                tx = subprocess.run([core,extra1,extra2,"move",user,target,amount],stdout=subprocess.PIPE)
+                                bot.send_message(chat_id=update.message.chat_id, text="@{0} paid @{1} of {2} SPERO".format(user, target, amount))
+                else:
+                        bot.send_message(chat_id=update.message.chat_id, text="Error that user is not applicable.")
 
 def funds(bot,update):
-	user = update.message.from_user.username
-	if user is None:
-		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
-	else:
-		core = "/home/sperocoin/DigitalCoinBRL/src/SperoCoind"
-		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
-		clean = (result.stdout.strip()).decode("utf-8")
-		balance  = float(clean)
-		balance =  str(round(balance,3))
-		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} SPERO ".format(user,balance))
+        user = update.message.from_user.username
+        if user is None:
+                bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+        else:
+                core = "/usr/local/bin/SperoCoind"
+                extra1 = "-datadir=/coin/data"
+                extra2 = "-conf=/coin/sperocoin.conf"
+                result = subprocess.run([core,extra1,extra2,"getbalance",user],stdout=subprocess.PIPE)
+                clean = (result.stdout.strip()).decode("utf-8")
+                balance  = float(clean)
+                balance =  str(round(balance,3))
+                bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} SPERO ".format(user,balance))
 
 
 
 def drawout(bot,update):
-	user = update.message.from_user.username
-	if user is None:
-		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
-	else:
-		target = update.message.text[9:]
-		address = target[:35]
-		address = ''.join(str(e) for e in address)
-		target = target.replace(target[:35], '')
-		amount = float(target)
-		core = "/home/sperocoin/DigitalCoinBRL/src/SperoCoind"
-		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
-		clean = (result.stdout.strip()).decode("utf-8")
-		balance = float(clean)
-		if balance < amount:
-			bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
-		else:
-			amount = str(amount)
-			tx = subprocess.run([core,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
-			cleantxid = (tx.stdout.strip()).decode("utf-8")
+        user = update.message.from_user.username
+        if user is None:
+                bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+        else:
+                target = update.message.text[9:]
+                address = target[:35]
+                address = ''.join(str(e) for e in address)
+                target = target.replace(target[:35], '')
+                amount = float(target)
+                core = "/usr/local/bin/SperoCoind"
+                extra1 = "-datadir=/coin/data"
+                extra2 = "-conf=/coin/sperocoin.conf"
+                result = subprocess.run([core,extra1,extra2,"getbalance",user],stdout=subprocess.PIPE)
+                clean = (result.stdout.strip()).decode("utf-8")
+                balance = float(clean)
+                if balance < amount:
+                        bot.send_message(chat_id=update.message.chat_id, text="@{0} you have insufficent funds.".format(user))
+                else:
+                        amount = str(amount)
+                        tx = subprocess.run([core,extra1,extra2,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
+                        cleantxid = (tx.stdout.strip()).decode("utf-8")
                         txid = str(cleantxid)
-                        bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} SPERO\n\n TXID: http://sperocoin.ddns.net:3001/tx/{3}" .format(user,address,amount,txid))
-
-
+                        bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} SPERO\n\n TXID: https://explorer.sperocoin.org/tx/{3}" .format(user,address,amount,txid))
 
 def hello(bot,update):
-	user = update.message.from_user.username
-	bot.send_message(chat_id=update.message.chat_id, text="Hello @{0}, how about buying some SPERO at Altilly?\n https://altilly.com".format(user))
+        user = update.message.from_user.username
+        bot.send_message(chat_id=update.message.chat_id, text="Hello @{0}, how about buying some SPERO at SPERO EXCHANGE?\n https://exchange.sperocoin.org".format(user))
 
 def rain(bot,update):
-  bot.send_message(chat_id=update.message.chat_id, text="Stake for a rainy day!")
+        bot.send_message(chat_id=update.message.chat_id, text="Stake for a rainy day!")
+
+def blocks(bot,update):
+        user = update.message.from_user.username
+        if user is None:
+                bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
+        else:
+                address = "/usr/local/bin/SperoCoind"
+                extra1 = "-datadir=/coin/data"
+                extra2 = "-conf=/coin/sperocoin.conf"
+                result = subprocess.run([address,extra1,extra2,"getblockcount"],stdout=subprocess.PIPE)
+                clean = (result.stdout.strip()).decode("utf-8")
+                bot.send_message(chat_id=update.message.chat_id, text="Bloco Atual da wallet do Bot: {0}".format(clean))
+
 
 def price(bot,update):
-	speroCapJson = requests.get('https://api.coingecko.com/api/v3/coins/sperocoin').json()
-	mk_cap = speroCapJson ['market_data']['market_cap']['brl']
-	pricebrl = speroCapJson ['market_data']['current_price']['brl']
-	priceusd = speroCapJson ['market_data']['current_price']['usd']
-	pricebtc = speroCapJson ['market_data']['current_price']['btc']
-	priceeth = speroCapJson ['market_data']['current_price']['eth']
-	update.message.reply_text("💵 Price: \n Cotação/Price: Coingecko \n SPERO Market Cap: R$:{:.2f}".format(mk_cap)+
-"\n Price(BRL):  R${:.3f}".format(pricebrl) + "\n Price(USD):  ${:.3f}".format(priceusd) + "\n Price(BTC):  {:.8f}".format(pricebtc) + "\n Price(ETH):  {:.8f}".format(priceeth))
+        speroCapJson = requests.get('https://exchange.sperocoin.org/api/v2/tickers.json').json()
+        sperobtc = speroCapJson ['sperobtc']['ticker']['last']
+	sperodoge = speroCapJson ['sperodoge']['ticker']['last']
+	ltcspero = speroCapJson ['ltcspero']['ticker']['last']
+	dashspero = speroCapJson ['dashspero']['ticker']['last']
+	fchspero = speroCapJson ['fchspero']['ticker']['last']
+	pwcspero = speroCapJson ['pwcspero']['ticker']['last']
+	babspero = speroCapJson ['babspero']['ticker']['last']
+	zyonspero = speroCapJson ['zyonspero']['ticker']['last']
+	niospero = speroCapJson ['niospero']['ticker']['last']
+	mic3spero = speroCapJson ['mic3spero']['ticker']['last']
+	xthspero = speroCapJson ['xthspero']['ticker']['last']
+        update.message.reply_text("💵 Price: \n Cotação/Price: Spero Exchange \n "+
+"\n SPERO/BTC:  R${:.8f}".format(sperobtc) + "\n SPERO/DOGE:  ${:.8f}".format(sperodoge) + "\n SPERO/LTC:  {:.8f}".format(ltcspero) + "\n SPERO/DASH:  {:.8f}".format(dashspero) + "\n SPERO/FCH:  ${:.8f}".format(fchspero) + "\n SPERO/PWC:  ${:.8f}".format(pwcspero) + "\n SPERO/BAB:  ${:.8f}".format(babspero) + "\n SPERO/ZYON:  ${:.8f}".format(zyonspero) + "\n SPERO/NIO:  ${:.8f}".format(niospero) + "\n SPERO/MIC3:  ${:.8f}".format(mic3spero) + "\n SPERO/XTH:  ${:.8f}".format(xthspero))
 
 
 from telegram.ext import CommandHandler
@@ -149,8 +176,10 @@ dispatcher.add_handler(balance_handler)
 help_handler = CommandHandler('help', help)
 dispatcher.add_handler(help_handler)
 
+price_handler = CommandHandler('blocks', blocks)
+dispatcher.add_handler(price_handler)
+
 price_handler = CommandHandler('price', price)
 dispatcher.add_handler(price_handler)
 
 updater.start_polling()
-
